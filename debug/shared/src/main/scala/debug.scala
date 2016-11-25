49160f2364ee20628f136566422e2472396607df
Alejandro Gómez <alejandro@dialelo.com>
2016-11-25 14:50:33 +0100

    Show exceptions with human-readable messages

--- debug/shared/src/main/scala/debug.scala
+++ debug/shared/src/main/scala/debug.scala
@@ -17,6 +17,7 @@
 package fetch
 
 object debug {
+  // TODO: better formatting
   import scala.text.Document
 
   def string(doc: Document): String = {
@@ -28,21 +29,24 @@ object debug {
   def pile(docs: Seq[Document]): Document =
     docs.foldLeft(Document.empty: Document)(_ :/: _)
 
-  def showEnv(env: FetchEnv): Document = {
-    val result = for {
-      last <- env.rounds.lastOption
-    } yield last.response
-    val resultDoc = result.fold(Document.empty: Document)((r) => Document.text(s"ing `${r}`"))
+  def showEnv(env: Env): Document = env.rounds match {
+    case Nil => Document.text("Empty environment")
+    case _ => {
+      val result = for {
+        last <- env.rounds.lastOption
+      } yield last.response
+      val resultDoc = result.fold(Document.empty: Document)((r) => Document.text(s"ing `${r}`"))
 
-    val duration = for {
-      first <- env.rounds.headOption
-      last  <- env.rounds.lastOption
-    } yield last.end - first.start
-    val durationDoc =
-      duration.fold(Document.empty: Document)((d) => Document.text(s" took ${d / 1e9} seconds"))
+      val duration = for {
+        first <- env.rounds.headOption
+        last  <- env.rounds.lastOption
+      } yield last.end - first.start
+      val durationDoc =
+        duration.fold(Document.empty: Document)((d) => Document.text(s" took ${d / 1e9} seconds"))
 
-    Document.text("Fetch") :: resultDoc :: durationDoc :/: Document
-      .nest(2, pile(env.rounds.map(showRound)))
+      Document.text("Fetch") :: resultDoc :: durationDoc :/: Document
+        .nest(2, pile(env.rounds.map(showRound)))
+    }
   }
 
   def showRound(r: Round): Document = r match {
@@ -57,12 +61,35 @@ object debug {
   }
 
   def showQuery(q: FetchQuery[_, _]): Document = q match {
-    case FetchOne(id, ds) => Document.text(s"[Fetch one] From `${ds.name}` with id `${id}`")
+    case FetchOne(id, ds) => Document.text(s"[Fetch one] From `${ds.name}` with id ${id}")
     case FetchMany(ids, ds) =>
-      Document.text(s"[Fetch many] From `${ds.name}` with ids `${ids.toList}`")
+      Document.text(s"[Fetch many] From `${ds.name}` with ids ${ids.toList}")
   }
 
-  def describe(env: FetchEnv) = {
+  def describe(env: Env) = {
     println(string(showEnv(env)))
+  }
+
+  def showMissing(ds: DataSourceName, ids: List[_]): Document = {
+    Document.text(s"`${ds}` missing identities ${ids}")
+  }
+
+  def showException(err: FetchException): Document = {
+    val excDoc = err match {
+      case NotFound(env, q @ FetchOne(id, ds)) =>
+        Document.text(s"[Error] Identity not found: ${id} in `${ds.name}`")
+      case MissingIdentities(env, missing) =>
+        Document.text("[Error] Missing identities") :/:
+          Document.nest(2, pile(missing.toSeq.map((kv) => showMissing(kv._1, kv._2))))
+      case UnhandledException(env, err) =>
+        Document.text(s"[Error] Unhandled `${err.getClass.getName}`: ${err.getMessage}")
+    }
+    excDoc :/: Document.break :/: Document.nest(2, showEnv(err.env)) :/: Document.break :/: Document.text(
+      s"Fetch finished after ${err.env.rounds.size} rounds")
+  }
+
+
+  def describe(err: FetchException) = {
+    println(string(showException(err) :/: showEnv(err.env)))
   }
 }
