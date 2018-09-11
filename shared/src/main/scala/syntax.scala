53f58568deacf6f9adf32b8ad41d239a8303795d
Alejandro Gómez <alejandro.gomez@47deg.com>
2018-09-11 12:47:48 +0200

    Syntax

---
+++ shared/src/main/scala/syntax.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2016-2018 47 Degrees, LLC. <http://www.47deg.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fetch
+
+import cats.effect._
+
+object syntax {
+
+  /** Implicit syntax to lift any value to the context of Fetch via pure */
+  implicit class FetchIdSyntax[A](val a: A) extends AnyVal {
+
+    def fetch: Fetch[A] =
+      Fetch.pure(a)
+  }
+
+
+  /** Implicit syntax to lift exception to Fetch errors */
+  implicit class FetchExceptionSyntax(val a: Throwable) extends AnyVal {
+
+    def fetch[B]: Fetch[B] =
+      Fetch.error[B](a)
+  }
+
+  /** Implicit syntax for Fetch ops */
+  implicit class FetchSyntax[A](val fa: Fetch[A]) extends AnyVal {
+    def runFetch(
+      implicit
+        CS: ContextShift[IO],
+        T: Timer[IO]
+    ): IO[A] =
+      Fetch.run(fa)
+
+    def runEnv(
+      implicit
+        CS: ContextShift[IO],
+        T: Timer[IO]
+    ): IO[(Env, A)] =
+      Fetch.runEnv(fa)
+
+    def runCache(
+      implicit
+        CS: ContextShift[IO],
+        T: Timer[IO]
+    ): IO[(DataSourceCache, A)] =
+      Fetch.runCache(fa)
+  }
+}
+
