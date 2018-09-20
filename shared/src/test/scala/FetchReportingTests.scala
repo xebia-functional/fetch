022098d5bf404a029ce757ff2a31486a635b0f0f
Alejandro Gómez <alejandro.gomez@47deg.com>
2018-09-20 17:22:45 +0200

    Clean tests

--- shared/src/test/scala/FetchReportingTests.scala
+++ shared/src/test/scala/FetchReportingTests.scala
@@ -22,6 +22,7 @@ import org.scalatest.{AsyncFreeSpec, Matchers}
 
 import fetch._
 
+import cats.temp.par._
 import cats.effect._
 import cats.instances.list._
 import cats.syntax.all._
@@ -34,7 +35,7 @@ class FetchReportingTests extends AsyncFreeSpec with Matchers {
   implicit val cs: ContextShift[IO] = IO.contextShift(executionContext)
 
   "Plain values have no rounds of execution" in {
-    def fetch[F[_] : ConcurrentEffect] =
+    def fetch[F[_] : ConcurrentEffect : Par] =
       Fetch.pure[F, Int](42)
 
     val io = Fetch.runEnv[IO](fetch)
@@ -45,7 +46,7 @@ class FetchReportingTests extends AsyncFreeSpec with Matchers {
   }
 
   "Single fetches are executed in one round" in {
-    def fetch[F[_] : ConcurrentEffect] =
+    def fetch[F[_] : ConcurrentEffect : Par] =
       one(1)
 
     val io = Fetch.runEnv[IO](fetch)
@@ -56,7 +57,7 @@ class FetchReportingTests extends AsyncFreeSpec with Matchers {
   }
 
   "Single fetches are executed in one round per binding in a for comprehension" in {
-    def fetch[F[_] : ConcurrentEffect] = for {
+    def fetch[F[_] : ConcurrentEffect : Par] = for {
       o <- one(1)
       t <- one(2)
     } yield (o, t)
@@ -69,7 +70,7 @@ class FetchReportingTests extends AsyncFreeSpec with Matchers {
   }
 
   "Single fetches for different data sources are executed in multiple rounds if they are in a for comprehension" in {
-    def fetch[F[_] : ConcurrentEffect] = for {
+    def fetch[F[_] : ConcurrentEffect : Par] = for {
       o <- one(1)
       m <- many(3)
     } yield (o, m)
@@ -82,7 +83,7 @@ class FetchReportingTests extends AsyncFreeSpec with Matchers {
   }
 
   "Single fetches combined with cartesian are run in one round" in {
-    def fetch[F[_] : ConcurrentEffect] =
+    def fetch[F[_] : ConcurrentEffect : Par] =
       (one(1), many(3)).tupled
 
     val io = Fetch.runEnv[IO](fetch)
@@ -93,7 +94,7 @@ class FetchReportingTests extends AsyncFreeSpec with Matchers {
   }
 
   "Single fetches combined with traverse are run in one round" in {
-    def fetch[F[_] : ConcurrentEffect] = for {
+    def fetch[F[_] : ConcurrentEffect : Par] = for {
       manies <- many(3)                 // round 1
       ones   <- manies.traverse(one[F]) // round 2
     } yield ones
@@ -106,7 +107,7 @@ class FetchReportingTests extends AsyncFreeSpec with Matchers {
   }
 
   "The product of two fetches from the same data source implies batching" in {
-    def fetch[F[_] : ConcurrentEffect] =
+    def fetch[F[_] : ConcurrentEffect : Par] =
       (one(1), one(3)).tupled
 
     val io = Fetch.runEnv[IO](fetch)
@@ -117,19 +118,19 @@ class FetchReportingTests extends AsyncFreeSpec with Matchers {
   }
 
   "The product of concurrent fetches of the same type implies everything fetched in batches" in {
-    def aFetch[F[_] : ConcurrentEffect] = for {
+    def aFetch[F[_] : ConcurrentEffect : Par] = for {
       a <- one(1)  // round 1
       b <- one(2)  // round 2
       c <- one(3)
     } yield c
 
-    def anotherFetch[F[_] : ConcurrentEffect] = for {
+    def anotherFetch[F[_] : ConcurrentEffect : Par] = for {
       a <- one(2)  // round 1
       m <- many(4) // round 2
       c <- one(3)
     } yield c
 
-    def fetch[F[_] : ConcurrentEffect] =
+    def fetch[F[_] : ConcurrentEffect : Par] =
       ((aFetch, anotherFetch).tupled, one(3)).tupled
 
     val io = Fetch.runEnv[IO](fetch)
