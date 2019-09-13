a1c44768baa23b0acd081c6a064a5007dd8341fc
Juan Pedro Moreno <juanpmorest@gmail.com>
2019-09-13 10:26:42 +0200

    updated site

--- js/main.js
+++ js/main.js
@@ -1,45 +1,78 @@
-// This initialization requires that this script is loaded with `defer`
-const navElement = document.querySelector("#navigation");
+jQuery(document).ready(function() {
+  hljs.initHighlightingOnLoad();
+  activeToggle();
+  loadGitHubStats();
+  linkifyAllLevels(".docs .content-wrapper");
+});
 
-/**
- * Toggle an specific class to the received DOM element.
- * @param {string}	elemSelector The query selector specifying the target element.
- * @param {string}	[activeClass='active'] The class to be applied/removed.
- */
-function toggleClass(elemSelector, activeClass = "active") {
-  const elem = document.querySelector(elemSelector);
-  if (elem) {
-    elem.classList.toggle(activeClass);
-  }
-}
 
-const scrollTop = Math.max(window.pageYOffset, document.documentElement.scrollTop, document.body.scrollTop)
-
-// Navigation element modification through scrolling
-function scrollFunction() {
-  if (window.pageYOffset || document.documentElement.scrollTop > scrollTop) {
-    navElement.classList.add("nav-scroll");
-  } else {
-    navElement.classList.remove("nav-scroll");
-  }
-}
-
-// Init call
-function loadEvent() {
-  document.addEventListener("scroll", scrollFunction);
-
-  const lottieAnimation = bodymovin.loadAnimation({
-          container: document.getElementById('fetch-animation'),
-          renderer: 'svg' / 'canvas' / 'html',
-          loop: true,
-          autoplay:true,
-          path: 'js/fetch_animation.json'
-  })
-
-  document.getElementById('fetch-animation').addEventListener('load', function() {
-    lottieAnimation.play();
+function activeToggle() {
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
   });
 }
 
-// Attach the functions to each event they are interested in
-window.addEventListener("load", loadEvent);
+var anchorForId = function (id) {
+  var anchor = document.createElement("a");
+  anchor.className = "header-link";
+  anchor.href      = "#" + id;
+  anchor.innerHTML = "<i class=\"fa fa-link\"></i>";
+  return anchor;
+};
+
+var linkifyAnchors = function (level, containingElement) {
+  var headers = containingElement.getElementsByTagName("h" + level);
+  for (var h = 0; h < headers.length; h++) {
+    var header = headers[h];
+
+    if (typeof header.id !== "undefined" && header.id !== "") {
+      header.appendChild(anchorForId(header.id));
+    }
+  }
+};
+
+var linkifyAllLevels = function (blockSelector) {
+  var contentBlock = document.querySelector(blockSelector);
+  if (!contentBlock) {
+    return;
+  }
+  for (var level = 1; level <= 6; level++) {
+    linkifyAnchors(level, contentBlock);
+  }
+};
+
+var baseURL = window.location.href;
+
+function shareSiteFacebook(text) {
+  launchPopup('http://www.facebook.com/sharer/sharer.php?u='+baseURL+'&t=' + text);
+}
+
+function shareSiteTwitter(text) {
+  launchPopup('https://twitter.com/home?status=' + text);
+  return false;
+}
+
+function shareSiteGoogle() {
+  launchPopup('https://plus.google.com/share?url='+baseURL);
+  return false;
+}
+
+function launchPopup(url) {
+  window.open(url, 'Social Share', 'height=320, width=640, toolbar=no, menubar=no, scrollbars=no, resizable=no, location=no, directories=no, status=no');
+}
+
+function loadGitHubStats() {
+  var content = $("#content");
+  var githubOwner = content.attr("data-github-owner")
+  var githubRepo = content.attr("data-github-repo")
+
+  if(githubOwner && githubRepo) {
+    var gitHubAPI = "https://api.github.com/repos/" + githubOwner + "/" + githubRepo + "?callback=?";
+    $.getJSON(gitHubAPI).done(function(data) {
+      $('#eyes').text(data.data.subscribers_count);
+      $('#stars').text(data.data.stargazers_count);
+    });
+  }
+
+}
