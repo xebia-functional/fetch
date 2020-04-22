75ff734f840a8cea454c5b37e86ff205b06770fd
Scala Steward <me@scala-steward.org>
2020-04-22 10:03:11 +0200

    Update cats-effect to 2.1.3

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -23,7 +23,7 @@ object ProjectPlugin extends AutoPlugin {
       Seq(
         libraryDependencies ++=
           Seq(
-            "org.typelevel" %% "cats-effect" % "2.0.0",
+            "org.typelevel" %% "cats-effect" % "2.1.3",
             "org.scalatest" %% "scalatest"   % "3.1.1" % "test"
           )
       )
