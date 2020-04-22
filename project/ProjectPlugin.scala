6341994f0649b89f999ffdf9c935b48dbd4fccf0
Juan Pedro Moreno <juanpmorest@gmail.com>
2020-04-22 10:24:59 +0200

    Removes useless interpolation

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -70,8 +70,8 @@ object ProjectPlugin extends AutoPlugin {
     lazy val examplesSettings = Seq(
       libraryDependencies ++= Seq(
         "io.circe"      %% "circe-generic"       % "0.13.0",
-        "org.tpolecat"  %% s"doobie-core"        % "0.9.0",
-        "org.tpolecat"  %% s"doobie-h2"          % "0.9.0",
+        "org.tpolecat"  %% "doobie-core"        % "0.9.0",
+        "org.tpolecat"  %% "doobie-h2"          % "0.9.0",
         "org.tpolecat"  %% "atto-core"           % "0.7.2",
         "org.http4s"    %% "http4s-blaze-client" % "0.21.3",
         "org.http4s"    %% "http4s-circe"        % "0.21.3",
