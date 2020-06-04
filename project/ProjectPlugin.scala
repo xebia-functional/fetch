ef579022048abd3b36f3dcac9ebd0dbefaddab1d
Scala Steward <me@scala-steward.org>
2020-06-05 00:03:11 +0200

    Update jedis to 3.3.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -67,7 +67,7 @@ object ProjectPlugin extends AutoPlugin {
         "org.tpolecat" %% "atto-core"           % "0.8.0",
         "org.http4s"   %% "http4s-blaze-client" % "0.21.4",
         "org.http4s"   %% "http4s-circe"        % "0.21.4",
-        "redis.clients" % "jedis"               % "2.10.2",
+        "redis.clients" % "jedis"               % "3.3.0",
         "io.monix"     %% "monix"               % "3.2.2"
       )
     ) ++ commonCrossDependencies
