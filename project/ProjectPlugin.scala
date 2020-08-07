b1cbfba47b17b0995733ae5858f44c60b519d887
47erbot <developer@47deg.com>
2020-08-07 07:07:12 +0200

    Update scalatest to 3.2.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -15,7 +15,7 @@ object ProjectPlugin extends AutoPlugin {
         libraryDependencies ++=
           Seq(
             "org.typelevel" %% "cats-effect" % "2.1.4",
-            "org.scalatest" %% "scalatest"   % "3.2.0" % "test"
+            "org.scalatest" %% "scalatest"   % "3.2.1" % "test"
           )
       )
 
