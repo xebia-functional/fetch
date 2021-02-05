83fc3ee01822f180f60e924334a5552961b38495
47erbot <developer@47deg.com>
2021-02-05 06:09:17 +0100

    Update http4s-blaze-client, http4s-circe to 0.21.18

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -65,8 +65,8 @@ object ProjectPlugin extends AutoPlugin {
         "org.tpolecat" %% "doobie-core"         % "0.10.0",
         "org.tpolecat" %% "doobie-h2"           % "0.10.0",
         "org.tpolecat" %% "atto-core"           % "0.9.0",
-        "org.http4s"   %% "http4s-blaze-client" % "0.21.17",
-        "org.http4s"   %% "http4s-circe"        % "0.21.17",
+        "org.http4s"   %% "http4s-blaze-client" % "0.21.18",
+        "org.http4s"   %% "http4s-circe"        % "0.21.18",
         "redis.clients" % "jedis"               % "3.5.1",
         "io.monix"     %% "monix"               % "3.3.0"
       )
