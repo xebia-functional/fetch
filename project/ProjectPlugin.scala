7a9a0e4ffd0405df9384412abb9e61d75f1130d7
47erbot <developer@47deg.com>
2022-05-20 07:16:59 +0200

    Update circe-generic to 0.14.2

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -61,7 +61,7 @@ object ProjectPlugin extends AutoPlugin {
 
     lazy val examplesSettings = Seq(
       libraryDependencies ++= Seq(
-        "io.circe"     %% "circe-generic"       % "0.14.1",
+        "io.circe"     %% "circe-generic"       % "0.14.2",
         "org.tpolecat" %% "doobie-core"         % "1.0.0-RC2",
         "org.tpolecat" %% "doobie-h2"           % "1.0.0-RC2",
         "org.tpolecat" %% "atto-core"           % "0.9.5",
