6de9a8d88de82f01b8328c1e8ea9fd7d15f6e6cb
47erbot <developer@47deg.com>
2022-07-13 07:19:05 +0200

    Update cats-effect to 3.3.14

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -15,7 +15,7 @@ object ProjectPlugin extends AutoPlugin {
       Seq(
         libraryDependencies ++=
           Seq(
-            "org.typelevel" %%% "cats-effect" % "3.3.13",
+            "org.typelevel" %%% "cats-effect" % "3.3.14",
             "org.scalatest" %%% "scalatest"   % "3.2.12" % "test"
           )
       )
