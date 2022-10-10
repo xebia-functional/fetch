184b44e119299625ce59b77fb329c19e9763663c
47erbot <developer@47deg.com>
2022-10-10 02:03:23 -0400

    Update jedis to 4.3.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -67,7 +67,7 @@ object ProjectPlugin extends AutoPlugin {
         "org.tpolecat" %% "atto-core"           % "0.9.5",
         "org.http4s"   %% "http4s-blaze-client" % "0.23.12",
         "org.http4s"   %% "http4s-circe"        % "0.23.16",
-        "redis.clients" % "jedis"               % "4.2.3"
+        "redis.clients" % "jedis"               % "4.3.0"
       )
     ) ++ commonCrossDependencies
   }
