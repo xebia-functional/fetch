654b670b60ad6226e0b9f1a5fd4c053be9667911
oss-47 <functional-ops@xebia.com>
2024-10-23 05:28:08 +0000

    Update circe-generic, circe-parser to 0.14.10

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -61,15 +61,15 @@ object ProjectPlugin extends AutoPlugin {
 
     lazy val examplesSettings = Seq(
       libraryDependencies ++= Seq(
-        "io.circe"     %% "circe-generic"       % "0.14.9",
+        "io.circe"     %% "circe-generic"       % "0.14.10",
         "org.tpolecat" %% "doobie-core"         % "1.0.0-RC5",
         "org.tpolecat" %% "doobie-h2"           % "1.0.0-RC5",
         "org.tpolecat" %% "atto-core"           % "0.9.5",
         "org.http4s"   %% "http4s-blaze-client" % "0.23.16",
         "org.http4s"   %% "http4s-circe"        % "0.23.27",
         "redis.clients" % "jedis"               % "5.1.4",
-        "io.circe"     %% "circe-parser"        % "0.14.9" % Test,
-        "org.slf4j"     % "slf4j-simple"        % "2.0.16" % Test
+        "io.circe"     %% "circe-parser"        % "0.14.10" % Test,
+        "org.slf4j"     % "slf4j-simple"        % "2.0.16"  % Test
       )
     ) ++ commonCrossDependencies
   }
