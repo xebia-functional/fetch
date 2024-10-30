d335c76c32d0580ef449d5cbdaa073df309ad1da
oss-47 <functional-ops@xebia.com>
2024-10-30 05:20:17 +0000

    Update jedis to 5.2.0

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -67,7 +67,7 @@ object ProjectPlugin extends AutoPlugin {
         "org.tpolecat" %% "atto-core"           % "0.9.5",
         "org.http4s"   %% "http4s-blaze-client" % "0.23.16",
         "org.http4s"   %% "http4s-circe"        % "0.23.29",
-        "redis.clients" % "jedis"               % "5.1.5",
+        "redis.clients" % "jedis"               % "5.2.0",
         "io.circe"     %% "circe-parser"        % "0.14.10" % Test,
         "org.slf4j"     % "slf4j-simple"        % "2.0.16"  % Test
       )
