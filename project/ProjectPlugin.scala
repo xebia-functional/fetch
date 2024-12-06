db8ad1a781a4610862ac98cabe22db47ec2eb8e0
oss-47 <functional-ops@xebia.com>
2024-12-06 05:13:00 +0000

    Update http4s-circe to 0.23.30

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -66,7 +66,7 @@ object ProjectPlugin extends AutoPlugin {
         "org.tpolecat" %% "doobie-h2"           % "1.0.0-RC5",
         "org.tpolecat" %% "atto-core"           % "0.9.5",
         "org.http4s"   %% "http4s-blaze-client" % "0.23.17",
-        "org.http4s"   %% "http4s-circe"        % "0.23.29",
+        "org.http4s"   %% "http4s-circe"        % "0.23.30",
         "redis.clients" % "jedis"               % "5.2.0",
         "io.circe"     %% "circe-parser"        % "0.14.10" % Test,
         "org.slf4j"     % "slf4j-simple"        % "2.0.16"  % Test
