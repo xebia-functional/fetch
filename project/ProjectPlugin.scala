d843e0dca6741871778dc3eae0e4e84a46a13f8a
Juan Pedro Moreno <juanpmorest@gmail.com>
2025-05-23 10:20:04 +0200

    Upgrades to circe 0.14.13

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -61,14 +61,14 @@ object ProjectPlugin extends AutoPlugin {
 
     lazy val examplesSettings = Seq(
       libraryDependencies ++= Seq(
-        "io.circe"     %% "circe-generic"       % "0.14.12",
+        "io.circe"     %% "circe-generic"       % "0.14.13",
         "org.tpolecat" %% "doobie-core"         % "1.0.0-RC9",
         "org.tpolecat" %% "doobie-h2"           % "1.0.0-RC9",
         "org.tpolecat" %% "atto-core"           % "0.9.5",
         "org.http4s"   %% "http4s-blaze-client" % "0.23.17",
         "org.http4s"   %% "http4s-circe"        % "0.23.30",
         "redis.clients" % "jedis"               % "6.0.0",
-        "io.circe"     %% "circe-parser"        % "0.14.12" % Test,
+        "io.circe"     %% "circe-parser"        % "0.14.13" % Test,
         "org.slf4j"     % "slf4j-simple"        % "2.0.17"  % Test
       )
     ) ++ commonCrossDependencies
