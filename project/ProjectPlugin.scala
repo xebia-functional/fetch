f0d057f2dc8c347228a574a552802d31141c0952
oss-47 <functional-ops@xebia.com>
2025-07-23 05:21:36 +0000

    Update cats-effect to 3.6.3

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -15,7 +15,7 @@ object ProjectPlugin extends AutoPlugin {
       Seq(
         libraryDependencies ++=
           Seq(
-            "org.typelevel" %%% "cats-effect" % "3.6.2",
+            "org.typelevel" %%% "cats-effect" % "3.6.3",
             "org.scalatest" %%% "scalatest"   % "3.2.19" % "test"
           )
       )
